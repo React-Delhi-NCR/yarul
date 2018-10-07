221af3974d94b7e669a500dfad4297175b2aff51
amandeep.bajwa <amandeep.bajwa@care.com>
2018-10-07 16:16:28 +0530

    Added Github & CSSMintHeader components

--- demo/src/app.js
+++ demo/src/app.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import demoImage from '../assets/tech.jpg';
-import { Alert } from '../../src/index';
+import { Alert, Header, CSSMintHeader } from '../../src/index';
 
 ReactDOM.render(
 	<div style={{ width: '500px', margin: '50px auto' }}>
 		<Alert type="primary" className="mt-20">
-			Primary Alert
+			Primary Alert!
 		</Alert>
+
+		<div className="box">
+		<h3>GitHub Header</h3>
+			<Header links={['one', 'two', 'three', 'four']}/>
+		</div>
+
+		<div className="box">
+			<h3>CSSMintHeader</h3>
+			<CSSMintHeader links={['one', 'two', 'three', 'four']} />
+		</div>
 	</div>,
 	document.getElementById('root')
 );
